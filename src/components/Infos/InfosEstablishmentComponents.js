e5137da30ca93faef9e05fd14063dff5bb665616
Sarah <sarah.morard@students.hevs.ch>
2020-11-05 09:38:33 +0100

    infoestablishment

--- src/components/Infos/InfosEstablishmentComponents.js
+++ src/components/Infos/InfosEstablishmentComponents.js
@@ -24,11 +24,19 @@ function InfosEstablishment(props) {
     /* State for the current establishment according to the id of the url*/
     const [poi, setPoi] = useState(null);
 
+    const modifInitial = {
+        name: null,
+        website: null,
+        deleted: false,
+        description: null,
+        address: null
+    }
+
+    /* State for set all the modifications in one */
+    const [modification, setModification] = useState(modifInitial);
+
     /* State for the text that can be copied with the copy button (sharing link of website) */
     const [copied, isCopied] = useState(false);
-
-    /* State for the modifiation that could have been done on one establishment */
-    const [ModificationsPois, setModificationsPois] = useState(null);
 
     /* State of the user for the rating of establishment */
     const [userVoted, setUserVoted] = useState(false);
@@ -52,69 +60,97 @@ function InfosEstablishment(props) {
 
 
     /* Method POST to rate one establishment (send idUser, idEstablishment, the rating) */
-    /*
+
     useEffect(() => {
         async function fetchUserVoted() {
-            let verifUserVoted = await Request(
-                `${process.env.REACT_APP_SERVER_URL}${endpoints.rating}` + "/" + context.user.githubId + "/" + eid,
-                getAccessTokenSilently,
-                loginWithRedirect
-            );
-            setUserVoted(verifUserVoted);
+            try {
+                let verifUserVoted = await Request(
+                    `${process.env.REACT_APP_SERVER_URL}${endpoints.userVoted}` + "/" + context.user.githubId + "/" + eid,
+                    getAccessTokenSilently,
+                    loginWithRedirect
+                );
+                setUserVoted(verifUserVoted);
+            } catch (err) {
+                console.error('problem verifiyng user vote', err);
+            }
         }
-        fetchUserVoted();
-    },[poi])
 
-     */
+        fetchUserVoted();
+    }, [poi])
 
 
     //Fetch all the right modification according to the id of the establishment
     useEffect(() => {
         async function fetchModif() {
-            let modif = await Request(
+            let modifications = await Request(
                 `${process.env.REACT_APP_SERVER_URL}${endpoints.modifications}` + "/" + eid,
                 getAccessTokenSilently,
                 loginWithRedirect
             );
-            setModificationsPois(modif);
+            await console.log("MOD: " + modifications)
+
+
+            let finalModif = {};
+            for (let modification of modifications) {
+                if (modification.name !== null) {
+                    finalModif.name = modification.name;
+                }
+                if (modification.description !== null) {
+                    finalModif.description = modification.description
+                }
+                if (modification.address !== null) {
+                    finalModif.address = modification.address
+                }
+
+                if (modification.website !== null) {
+                    finalModif.website = modification.website
+                }
+
+                if (modification.deleted !== null) {
+                    finalModif.deleted = modification.deleted
+                }
+            }
+
+            setModification(finalModif);
+
+
         }
-        console.log("MODIFICATIONS: " + ModificationsPois)
         fetchModif();
-    }, eid);
+    },[eid]);
 
 
-    /* Method POST to rate one establishment (send idUser, idEstablishment, the rating) */
-    const rateEstablishment = async() => {
+/* Method POST to rate one establishment (send idUser, idEstablishment, the rating) */
+const rateEstablishment = async () => {
 
-        // If the user has note voted we had a vote
-        console.log("USERVOTED : " + userVoted)
+    // If the user has note voted we had a vote
+    console.log("USERVOTED : " + userVoted)
 
-        if (userVoted == false) {
-            const data =
-                {
-                    establishment: poi,
-                    user: context.user,
-                    grade: stares
-                };
+    if (userVoted == false) {
+        const data =
+            {
+                establishment: poi,
+                user: context.user,
+                grade: stares
+            };
 
-            await Request(
-                `${process.env.REACT_APP_SERVER_URL}${endpoints.rating}`,
-                getAccessTokenSilently,
-                loginWithRedirect,
-                "POST",
-                data
-            )
-        } else {
-            alert("You have already voted for this establishment!")
-        }
+        await Request(
+            `${process.env.REACT_APP_SERVER_URL}${endpoints.rating}`,
+            getAccessTokenSilently,
+            loginWithRedirect,
+            "POST",
+            data
+        )
+    } else {
+        alert("You have already voted for this establishment!")
     }
+}
 
-    /** MODIFICATION METHODS **/
+/** MODIFICATION METHODS **/
     //Method POST to confirm the modifications of one establishment
-    const comfirmModif = async() => {
+const comfirmModif = async () => {
         //call the db with id establsihment and to insert the modification in the establishment table
         const data = {
-            modifId: ModificationsPois.establishmentId,
+            modifId: poi.establishmentId,
             validated: 1
         };
 
@@ -127,107 +163,122 @@ function InfosEstablishment(props) {
         );
     }
 
-    //Method to discard the modifications of one establishment
-    const discardModif = async () => {
-        const data = {
-            //Delete the modifications in the table Modification according to the id of the establishment
-            modifId: ModificationsPois.establishmentId,
-            validated: -1
-        };
+//Method to discard the modifications of one establishment
+const discardModif = async () => {
+    const data = {
+        //Delete the modifications in the table Modification according to the id of the establishment
+        modifId: poi.establishmentId,
+        validated: -1
+    };
 
-        await Request(
-            `${process.env.REACT_APP_SERVER_URL}${endpoints.modifications}`,
-            getAccessTokenSilently,
-            loginWithRedirect,
-            "PUT",
-            data
-        );
-    }
-
-
-    /** HANDLING METHODS **/
-    /* Rate the establishment and call the method to update the rating of the establishment */
-    const handleRatingChange = (newRating) => {
-        setStares(newRating);
-    }
+    await Request(
+        `${process.env.REACT_APP_SERVER_URL}${endpoints.modifications}`,
+        getAccessTokenSilently,
+        loginWithRedirect,
+        "PUT",
+        data
+    );
+}
 
 
-    /* Method to set the state of the copied value to copy the link of the website */
-    const handleCopy = () => {
-        isCopied(true);
-    }
-
-    const modifyEstablishment = () => {
-        props.modif(poi);
-        history.push("/modify");
-    }
+/** HANDLING METHODS **/
+/* Rate the establishment and call the method to update the rating of the establishment */
+const handleRatingChange = (newRating) => {
+    setStares(newRating);
+}
 
 
-    return (
-        <div className="infoEstablishment" style={{width: props.display ? '20%' : '0%'}}>
-            <a href="javascript:void(0)" className="infosClosebtn" onClick={() => history.push("/location")}>&times;</a>
-            {poi != null &&
-            <div className="infosContainer">
-                {ModificationsPois.name != null ?
-                    <h3 style={{color: "red"}}>{ModificationsPois.name}</h3>
-                    :
-                    <h3>{poi.name}</h3>
-                }
+/* Method to set the state of the copied value to copy the link of the website */
+const handleCopy = () => {
+    isCopied(true);
+}
 
-                {ModificationsPois.description != null ?
-                    <h3 style={{color:"red"}}>{ModificationsPois.description}</h3>
-                    :
-                    <h3>{poi.description}</h3>
-                }
+const modifyEstablishment = () => {
+    history.push("/modify");
+}
 
-                {ModificationsPois.address != null ?
-                    <h3 style={{color:"red"}}>{ModificationsPois.address}</h3>
-                    :
-                    <h3>{poi.address}</h3>
-                }
-
-                 <div className="stars">
-                        <ReactStars
-                            count={5}
-                            onChange={handleRatingChange}
-                            size={25}
-                            isHalf={false}
-                            emptyIcon={<i className="far fa-star"></i>}
-                            halfIcon={<i className="fa fa-star-half-alt"></i>}
-                            fullIcon={<i className="fa fa-star"></i>}
-                            activeColor="#ffd700"
-                        />
-                    </div>
-
-                <h3>Web Site:</h3>
-                {ModificationsPois.website != null ?
-                    <a href={"https://www.technopole.ch/"} target="_blank">{ModificationsPois.webSite}</a>
-                    :
-                    <a href={"https://www.technopole.ch/"} target="_blank" style={{color: "red"}}>{poi.webSite}</a>
-                }
-                    <br/>
-                <div className="btnInfoContainer">
-                    <CopyToClipboard
-                        text={window.location.href}
-                        onCopy={handleCopy}
-                    >
-                        <button>Copy Link</button>
-                    </CopyToClipboard>
+const deleteEstablishment = () => {
+    //Delete establishment
+}
 
 
-                        <button onClick={rateEstablishment}>Send rating</button>
-                        <button onClick={discardModif}>Discard Changes</button>
-                        <button onClick={comfirmModif}>Confirm Changes</button>
-                        <button onClick={modifyEstablishment}>Modify</button>
-                    </div>
-                </div>
+return (
+    <div className="infoEstablishment" style={{width: props.display ? '20%' : '0%'}}>
+        <a href="javascript:void(0)" className="infosClosebtn" onClick={() => history.push("/location")}>&times;</a>
+        {poi != null &&
+        <div className="infosContainer">
+
+            {modification.name != null ?
+                <h3 style={{color: "red"}}>{modification.name}</h3>
+                :
+                <h3>{poi.name}</h3>
             }
 
+            {modification.description != null ?
+                <h3 style={{color: "red"}}>{modification.description}</h3>
+                :
+                <h3>{poi.description}</h3>
+
+            }
+
+
+            {modification.address != null ?
+                <h3 style={{color: "red"}}>{modification.address}</h3>
+                :
+                <h3>{poi.address}</h3>
+
+            }
+
+            <div className="stars">
+                <ReactStars
+                    count={5}
+                    onChange={handleRatingChange}
+                    size={25}
+                    isHalf={false}
+                    emptyIcon={<i className="far fa-star"></i>}
+                    halfIcon={<i className="fa fa-star-half-alt"></i>}
+                    fullIcon={<i className="fa fa-star"></i>}
+                    activeColor="#ffd700"
+                />
+            </div>
+
+            <h3>Web Site:</h3>
+            {modification.website != null ?
+                <a href={"https://www.technopole.ch/"} target="_blank" style={{color: "red"}}>{modification.webSite}</a>
+                :
+                <a href={"https://www.technopole.ch/"} target="_blank">{poi.webSite}</a>
+            }
+
+            {modification.deleted != false ?
+                <p style={{color: "red"}}>The establishment does not exist anymore</p>
+                :
+                null
+            }
+
+            <br/>
+            <div className="btnInfoContainer">
+                <CopyToClipboard
+                    text={window.location.href}
+                    onCopy={handleCopy}
+                >
+                    <button>Copy Link</button>
+                </CopyToClipboard>
+
+
+                <button onClick={rateEstablishment}>Send rating</button>
+                <button onClick={discardModif}>Discard Changes</button>
+                <button onClick={comfirmModif}>Confirm Changes</button>
+                <button onClick={modifyEstablishment}>Modify</button>
+                <button onClick={deleteEstablishment}>Delete</button>
+
+            </div>
         </div>
-        );
+        }
 
-    }
+    </div>
+);
 
+}
 
 
 export default InfosEstablishment;
